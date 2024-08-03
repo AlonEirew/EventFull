9064e402214c34b7a3acd7300bb8d9484121c901
AlonEirew <alon.eirew@gmail.com>
2024-08-03 18:03:50 +0300

    wip refactor

--- public/forms/temporal.js
+++ public/forms/temporal.js
@@ -2,9 +2,11 @@
 // ############### temporal functions ###############
 // ####################################################
 
-class TemporalForm extends PairsForm {
+class TemporalForm extends UIForm {
     constructor(pageIndex, allAxes) {
-        super(pageIndex, allAxes, null, FormType.TEMPORAL);
+        super(pageIndex, allAxes, null);
+        this.formType = FormType.TEMPORAL;
+        this._selectedNodes = [];
     }
 
     getInstructions() {
@@ -13,7 +15,139 @@ class TemporalForm extends PairsForm {
 
     loadForm() {
         this._annotations = this.initTmpPairs();
+        if (this._annotationIndex !== 0 && this._annotationIndex >= this._annotations.length) {
+            this._annotationIndex = this._annotations.length - 1;
+        }
+
+        toggleGraphDivOn();
+        renderGraph(this);
         super.loadForm();
+    }
+
+    createUI() {
+        cleanQuestions();
+        let pair = null;
+        if (this._annotations.length > 0) {
+            pair = this._annotations[this._annotationIndex];
+        }
+
+        const questions = document.getElementById("questions");
+        const summaryPanel = document.createElement("div");
+        const buttonBackTask = this.createPrevTaskButton();
+        const buttonNextTask = this.createNextTaskButton();
+        const paragraph = document.createElement("p");
+        paragraph.style.backgroundColor = "#EBF5FB";
+
+        if (pair != null) {
+            paragraph.innerHTML = this.formatText(pair);
+            summaryPanel.appendChild(paragraph);
+
+            const divQuestion1 = this.getQuestion(pair);
+            summaryPanel.appendChild(divQuestion1);
+            summaryPanel.appendChild(document.createElement("br"));
+            summaryPanel.appendChild(buttonBackTask);
+            summaryPanel.appendChild(this.createBackButton("Back"));
+            summaryPanel.appendChild(this.createNextButton("Next"));
+
+            let nextUnhldButt = this.createUnhandledNextButton("Next Unhandled Pair");
+            if (pair.getRelation() !== EventRelationType.NA) {
+                nextUnhldButt.disabled = false;
+            }
+
+            summaryPanel.appendChild(nextUnhldButt);
+            summaryPanel.appendChild(buttonNextTask);
+
+            if (config.app.showRemainingAnnot === true) {
+                summaryPanel.appendChild(this.getAnnotationsRemainderElem());
+            }
+
+            questions.appendChild(summaryPanel);
+        } else {
+            paragraph.innerHTML = "<p><b>All done with this annotation task! You can proceed to the next task.</b></p>";
+            questions.appendChild(paragraph);
+            questions.appendChild(buttonBackTask);
+            questions.appendChild(buttonNextTask);
+        }
+
+        let nodes = refreshGraphElem(this.formType);
+        highlightCurrentPair(pair);
+        // check if nodes already listening to click event
+        if (nodes[0].emitter().listeners.length === 0) {
+            nodes.on('click', function (event) {
+                const curPage = pages[currentPageIdx];
+                if (this.animated()) {
+                    curPage._selectedNodes.splice(curPage._selectedNodes.indexOf(this.id()), 1);
+                    this.stop(true);
+                    this.animate({style: {'opacity': 1}});
+                } else if (curPage._selectedNodes.length === 1) {
+                    curPage._selectedNodes.push(this.id());
+                    curPage.handleManualNodeSelection();
+                    nodes.stop(true);
+                    nodes.animate({style: {'opacity': 1}});
+                    curPage._selectedNodes = [];
+                    curPage.createUI();
+                } else {
+                    curPage._selectedNodes.push(this.id());
+                    TemporalForm.blinkNode(this);
+                }
+            });
+        }
+    }
+
+    static blinkNode(node) {
+        return (
+            node.animation({
+                style: {'opacity': 0.3}
+            }, {
+                duration: 700,
+                complete: function () {
+                    // Reset the node's style after the animation is complete
+                    node.animation({style: {'opacity': 1}}, {duration: 700}).play();
+                    TemporalForm.blinkNode(node);
+                }
+            })).play();
+    }
+
+    getAnnotationsRemainderElem() {
+        const anchorRemains = this.annotationRemainder();
+        const countPer = document.createElement("p");
+        countPer.style.color = "red";
+        countPer.innerHTML = "Remaining relations to annotate = " + anchorRemains;
+        return countPer;
+    }
+
+    formatText(pair) {
+        let text = [...this._allAxes.getMainDocTokens()];
+        const allEvents = this._allAxes.getAllRelEvents();
+        const allTimeExpressions = this._allAxes.getAllTimeExpressions();
+        let event1 = this._allAxes.getEventByEventId(pair.getFirstId());
+        let event2 = this._allAxes.getEventByEventId(pair.getSecondId());
+        let start1Idx = event1.getTokensIds()[0];
+        let end1Idx = event1.getTokensIds().at(-1);
+
+        for (let i = 0; i < allEvents.length; i++) {
+            const eventStartIds = allEvents[i].getTokensIds()[0];
+            const eventEndIds = allEvents[i].getTokensIds().at(-1);
+            for (let i = eventStartIds; i <= eventEndIds; i++) {
+                text[i] = `<span style=\"font-weight: bold;\">${text[i]}</span>`;
+            }
+        }
+
+        for (let i = 0; i < allTimeExpressions.length; i++) {
+            text[allTimeExpressions[i]] = `<span style=\"text-decoration: underline;\">${text[allTimeExpressions[i]]}</span>`;
+        }
+
+        for (let i = start1Idx; i <= end1Idx; i++) {
+            text[i] = `<span class=\"label ANC\" \">${text[i]}</span>`;
+        }
+
+        let start2Idx = event2.getTokensIds()[0];
+        let end2Idx = event2.getTokensIds().at(-1);
+        for (let i = start2Idx; i <= end2Idx; i++) {
+            text[i] = `<span class=\"label NOT\">${text[i]}</span>`;
+        }
+
+        return text.join(" ");
     }
 
     handleSelection() {
@@ -43,6 +177,29 @@ class TemporalForm extends PairsForm {
         return retVal;
     }
 
+    handleDiscrepancies(discrepancy) {
+        const disRootEdge = this._allAxes.getEventByEventId(discrepancy[0]).getTokens();
+        const disOtherEdge = this._allAxes.getEventByEventId(discrepancy[1]).getTokens();
+        const viaNode = this._allAxes.getEventByEventId(discrepancy[2]).getTokens();
+        const currentRelation = discrepancy[3];
+        const inferredRelation = discrepancy[4];
+
+        Swal.fire({
+            icon: "error",
+            title: 'Discrepancy Alert',
+            html:
+                '<p>Your last selection has created a discrepancy between two events.<br/><br/>The relation currently set between the events: <span style=\"color:orangered; font-weight: bold;\">' + disRootEdge +'</span> and ' +
+                '<span style=\"color:orangered; font-weight: bold;\">' + disOtherEdge + '</span> is <span style=\"color:royalblue; font-weight: bold;\">' +
+                currentRelation + '</span>. However, due to the last selection, the events can now be inferred indirectly via event ' +
+                '<span style=\"color:green; font-weight: bold;\">' + viaNode + '</span> as also having a ' +
+                '<span style=\"color:royalblue; font-weight: bold;\">' + inferredRelation + '</span> relation.<br/><br/>' +
+                '<span style=\"font-weight: bold;\">Please fix or contact the task admin for help.</p>',
+            showCancelButton: false,
+            confirmButtonText: 'OK',
+            allowOutsideClick: false,
+            scrollbarPadding: true
+        });
+    }
 
     isRelationChanged(currentRelation, newRelation) {
         if(newRelation === EventRelationType.EQUAL) {
@@ -196,6 +353,19 @@ class TemporalForm extends PairsForm {
         return count;
     }
 
+    isFinalized() {
+        let allRelAxes = this._allAxes.getAllRelAxes();
+        for (let i = 0; i < allRelAxes.length; i++) {
+            let discrepancies = allRelAxes[i].getAxisGraph().getFormTransitiveAndDiscrepancies(this.formType)[1];
+            if (discrepancies.length > 0) {
+                this.handleDiscrepancies(discrepancies[i]);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     initTmpPairs() {
         const allAxesEvents = this._allAxes.getAllAxesEventsSorted()
         if(allAxesEvents == null || allAxesEvents.length === 0) {
@@ -235,7 +405,6 @@ class TemporalForm extends PairsForm {
         graphEventsToPreset = AllAxes.sortEventsByIndex(eventsToPresent);
         return allPairsFlat;
     }
-
 
     handleManualNodeSelection() {
         let index = this.findPair(this._annotations);
